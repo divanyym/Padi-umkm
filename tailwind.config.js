7585f14155850ce23486b194b6d1072a42c7fd37
divanyym <divanydyna09@gmail.com>
2024-09-07 16:05:13 +0700

    fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,10 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    './src/**/*.{js,jsx,ts,tsx}', // Pastikan semua file komponen dimasukkan
+    './public/index.html',
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
